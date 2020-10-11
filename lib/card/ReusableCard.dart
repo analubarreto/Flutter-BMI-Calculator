27da26d3db33b2cb6edb2771f4f59a5b49d2f0db
Ana Luiza <analuibm@gmail.com>
2020-10-10 17:06:00 -0700

    added button actions

--- lib/card/ReusableCard.dart
+++ lib/card/ReusableCard.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
 class ReusableCard extends StatelessWidget {
-  ReusableCard({ this.bgColor, this.cardChild });
+  ReusableCard({ this.bgColor, this.cardChild, this.onPress });
 
   final Color bgColor;
   final Widget cardChild;
+  final Function onPress;
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       flex: 2,
-      child: Container (
-        child: cardChild,
-        margin: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-          color: bgColor,
-          borderRadius: BorderRadius.circular(10.0)
+      child: GestureDetector(
+          onTap: onPress,
+          child: Container (
+          child: cardChild,
+          margin: EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+            color: bgColor,
+            borderRadius: BorderRadius.circular(10.0)
+          ),
         ),
       ),
     );
--- lib/pages/InputPage.dart
+++ lib/pages/InputPage.dart
@@ -1,10 +1,11 @@
-import 'package:bmi_calulator/card/cardIcon.dart';
+import 'package:bmi_calulator/card/CardIcon.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../card/ReusableCard.dart';
 
 const bottomContainerHeight = 80.0;
 const cardBgColor = Color(0xFF1D1E33);
+const inactiveCardBgColor = Color(0xFF111328);
 const bottomContainerColor = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
@@ -17,6 +18,30 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+
+  Color maleCardColor = inactiveCardBgColor;
+  Color femaleCardColor = inactiveCardBgColor;
+
+  void updateColor(int gender) {
+    if (gender == 1) {
+      if (maleCardColor == inactiveCardBgColor) {
+        maleCardColor = cardBgColor;
+        femaleCardColor = inactiveCardBgColor;
+      } else {
+        maleCardColor = inactiveCardBgColor;
+      }
+    }
+
+    if (gender == 2) {
+      if (femaleCardColor == inactiveCardBgColor) {
+        femaleCardColor = cardBgColor;
+        maleCardColor = inactiveCardBgColor;
+      } else {
+        femaleCardColor = inactiveCardBgColor;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,18 +54,28 @@ class _InputPageState extends State<InputPage> {
             child: Row (
               children: <Widget>[
                 ReusableCard(
-                  bgColor: cardBgColor,
+                  bgColor: maleCardColor,
                   cardChild: CardIcon(
                     iconImg: FontAwesomeIcons.mars,
                     iconText: 'MALE'
                   ),
+                  onPress: () => {
+                    setState(() => {
+                      updateColor(1),
+                    })
+                  },
                 ),
                 ReusableCard(
-                  bgColor: cardBgColor,
+                  bgColor: femaleCardColor,
                   cardChild: CardIcon(
                     iconImg: FontAwesomeIcons.venus,
                     iconText: 'FEMALE'
                   ),
+                  onPress: () => {
+                    setState(() => {
+                      updateColor(2),
+                    })
+                  },
                 ),
               ],
             ),
